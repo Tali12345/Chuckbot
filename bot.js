8eb11a6bb3e35ec713d69defacd446ee52ede720
Tali <123tali12345@gmail.com>
2023-11-17 15:41:23 +0200

    Add class Bot in bot.js file

--- bot.js
+++ bot.js
@@ -6,60 +6,65 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
 
-let currentLanguage = "en";
-
-function startMessage(msg) { 
-    return(
-        "Hello " + msg.from.first_name + " 😀,\n" +
-        "I'm Chuck Norris Bot. I support those options:\n" + 
-        "1. You can choose language by using the command 'set language [chosen language]'\n" +
-        "2. You can enter a number from 1 to 101, and I'll tell you this joke\n" + 
-        "Have fun! 🤣"
-    )
-}
-
-async function SetLanguage(msg) {
-    const userTextArray = msg.text.split(" ");
-    const language = userTextArray[userTextArray.length - 1];
-    const languageCode = translator.languageNameToCode(language);
-    if (languageCode==="null") {
-        return "Invalid language, use valid language for example 'set language hebrew'";
+class Bot {
+    
+    constructor(jokes) {
+        this.jokes = jokes;
+        this.currentLanguage = "en";
     }
-    const supportedLanguageSet = await translator.fetchSupportedLanguages();
-    if (!supportedLanguageSet.has(languageCode)) {
-        return "Unsupported language, use supported language for example 'set language hebrew'";
+
+    startMessage(msg) { 
+        return(
+            "Hello " + msg.from.first_name + " 😀,\n" +
+            "I'm Chuck Norris Bot. I support those options:\n" + 
+            "1. You can choose language by using the command 'set language [chosen language]'\n" +
+            "2. You can enter a number from 1 to 101, and I'll tell you this joke\n" + 
+            "Have fun! 🤣"
+        )
     }
-    currentLanguage = languageCode;
-    return "No problem";
-}
 
-
-async function GetJoke(jokes, msg) {
-    const jokeNumber = parseInt(msg.text);
-    if (jokeNumber>=1 && jokeNumber<=101) {
-        return msg.text+". "+jokes[jokeNumber-1];
-    }
-    return "The number should be from 1 to 101";
-}
-
-async function botResponse(jokes) {
-    bot.on('message', async (msg) => {
-        let response = "";
-        if (/\/start/.test(msg.text)) {
-            response = startMessage(msg);
-        } 
-        else if (/^set language (.+)$/.test(msg.text)) {
-            response = await SetLanguage(msg);
-        } 
-        else if (/^[0-9]\d*/.test(msg.text)) {
-            response = await GetJoke(jokes, msg);
-        } 
-        else {
-            response = "Enter 'set language [chosen language]' or a number from 1 to 101";
+    async SetLanguage(msg) {
+        const userTextArray = msg.text.split(" ");
+        const language = userTextArray[userTextArray.length - 1];
+        const languageCode = translator.languageNameToCode(language);
+        if (languageCode==="null") {
+            return "Invalid language, use valid language for example 'set language hebrew'";
         }
-        const translatedResponse = await translator.translate(response, currentLanguage);
-        bot.sendMessage(msg.chat.id, translatedResponse);
-    });
+        const supportedLanguageSet = await translator.fetchSupportedLanguages();
+        if (!supportedLanguageSet.has(languageCode)) {
+            return "Unsupported language, use supported language for example 'set language hebrew'";
+        }
+        this.currentLanguage = languageCode;
+        return "No problem";
+    }
+
+    async GetJoke(msg) {
+        const jokeNumber = parseInt(msg.text);
+        if (jokeNumber>=1 && jokeNumber<=101) {
+            return msg.text+". "+this.jokes[jokeNumber-1];
+        }
+        return "The number should be from 1 to 101";
+    }
+
+    async botResponse() {
+        bot.on('message', async (msg) => {
+            let response = "";
+            if (/\/start/.test(msg.text)) {
+                response = this.startMessage(msg);
+            } 
+            else if (/^set language (.+)$/.test(msg.text)) {
+                response = await this.SetLanguage(msg);
+            } 
+            else if (/^[0-9]\d*/.test(msg.text)) {
+                response = await this.GetJoke(msg);
+            } 
+            else {
+                response = "Enter 'set language [chosen language]' or a number from 1 to 101";
+            }
+            const translatedResponse = await translator.translate(response, this.currentLanguage);
+            bot.sendMessage(msg.chat.id, translatedResponse);
+        });
+    }
 }
 
-module.exports = { botResponse };
+module.exports = Bot;
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const bot = require('./bot');
+const Bot = require('./bot');
 const extractJokes = require('./extractJokes');
 
 async function main() {
     try {
         const jokes = await extractJokes.getQuotes();
         console.log("The jokes were extracted successfully");
-        await bot.botResponse(jokes);
+        const chuckBot = new Bot(jokes);
+        console.log("The bot is ready")
+        await chuckBot.botResponse();
     } catch (error) {
         console.error(error);
     }
